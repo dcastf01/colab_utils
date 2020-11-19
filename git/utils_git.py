8c3616e5aae8b8632542147a16a17ff4527d9657
David Castellano Falcón <dcastf01@gmail.com>
2020-11-19 10:19:58 +0100

    Update utils_git.py

--- git/utils_git.py
+++ git/utils_git.py
@@ -14,36 +14,41 @@ Load a json with the next format and download the repository and you can do comm
 
 
 from google.colab import files
+import os
 import subprocess 
-
+import json
 def upload_credential():
-  global GIT_USERNAME
-  global GIT_EMAIL
-  global GIT_TOKEN
-  global GIT_REPOSITORY
-  global PROJECT_PATH
+
   
   uploaded = files.upload()
   
   for file_with_credential in uploaded.keys():
     print('User uploaded file "{name}" with length {length} bytes'.format(
         name=file_with_credential, length=len(uploaded[file_with_credential])))
-  import json
+
+
 
   with open(file_with_credential, "r") as content:
     credential=json.load(content)
 
-  GIT_USERNAME=credential["GIT_USERNAME"]
-  GIT_EMAIL=credential["GIT_EMAIL"]
-  GIT_TOKEN=credential["GIT_TOKEN"]
-  GIT_REPOSITORY=credential["GIT_REPOSITORY"]
-  PROJECT_PATH=credential["PROJECT_PATH"]
 
-  rc = subprocess.call("download_repository.sh", shell=True)
-  
-def do_commit():
-  %cd /content/$GIT_REPOSITORY
-  !git add .
-  !git commit -m 'add imports'
-  !git push origin master 
-  %cd /content
+  print( "set env variables")
+  GIT_USERNAME=credential["GIT_USERNAME"]
+  os.environ["GIT_USERNAME"]=GIT_USERNAME
+  GIT_EMAIL=credential["GIT_EMAIL"]
+  os.environ["GIT_EMAIL"]=GIT_EMAIL
+  GIT_TOKEN=credential["GIT_TOKEN"]
+  os.environ["GIT_TOKEN"]=GIT_TOKEN
+  GIT_REPOSITORY=credential["GIT_REPOSITORY"]
+  os.environ["GIT_REPOSITORY"]=GIT_REPOSITORY
+  PROJECT_PATH=credential["PROJECT_PATH"]
+  os.environ["PROJECT_PATH"]=PROJECT_PATH
+  command="bash /content/colab_utils/git/download_repository.sh"
+  subprocess.run(command.split())
+
+# def do_commit():
+#   %cd /content/$GIT_REPOSITORY
+#   !git add .
+#   !git commit -m 'add imports'
+#   !git push origin master 
+#   %cd /content
