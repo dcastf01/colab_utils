98e0ca669a041ba134f97d95fd3ec9d617874f98
David Castellano Falcón <dcastf01@gmail.com>
2020-12-14 20:13:09 +0100

    Update Notification.py

--- utis/Notification.py
+++ utis/Notification.py
@@ -1,9 +1,10 @@
 #source https://github.com/hassaku/colab-a11y-utils
 
-
 from pydub import AudioSegment
 from pydub.generators import Sine, Pulse, Square, Sawtooth, Triangle, WhiteNoise
 from IPython.display import Audio, display
+
+from tqdm.notebook import tqdm as original_tqdm
 
 
 class InvisibleAudio(Audio):
@@ -13,16 +14,14 @@ class InvisibleAudio(Audio):
         return f'<div style="display:none">{audio}</div>'
 
 
-def set_sound_notification():
+def set_sound_notification2():
     def sound_notification_before(*args):
         sound = Triangle(440).to_audio_segment(duration=50).apply_gain(-10).fade_in(20).fade_out(20)
         display(InvisibleAudio(data=sound.export().read(), autoplay=True))
 
     def sound_notification_after(*args):
-        sound = Triangle(440).to_audio_segment(duration=50).apply_gain(-10).fade_in(20).fade_out(20)
-        sound += AudioSegment.silent(duration=100)
-        sound += Triangle(440).to_audio_segment(duration=300).apply_gain(-10).fade_in(20).fade_out(20)
+        sound=AudioSegment.from_mp3("/content/soundtrack_harry_potter.mp3")
         display(InvisibleAudio(data=sound.export().read(), autoplay=True))
 
-    get_ipython().events.register('pre_run_cell', sound_notification_before)
-    get_ipython().events.register('post_run_cell', sound_notification_after)
+    # get_ipython().events.register('pre_run_cell', sound_notification_before)
+    sound_notification_after()
